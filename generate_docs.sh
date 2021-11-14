1dc6223b766f11f27ba7dd441780634e677cadb8
Matthew Martin <mmartin@loc.gov>
2021-11-14 12:42:12 -0500

    can it do just one module

--- generate_docs.sh
+++ generate_docs.sh
@@ -3,7 +3,15 @@
 # The -w option appears to only write in the current directory
 # This will pollute the root with *.html files
 # It will however, generate docs for all submodules
+# It will also generate docs for the tests folder
 python -m pydoc -w .\\
+
+# Attempt 1 to generate docs for just cats
+#$ python -m pydoc -w cats.\\
+#wrote __main__.html
+#No Python documentation found for 'data'.
+#Use help() to get the interactive help utility.
+#Use help(str) for help on the str class.
 
 # no obvious way to tell python to put these directly into docs folder
 # mv *.html docs
---
+++ tests/test_it.py
@@ -0,0 +1,4 @@
+import cats.data.cat_data as data
+
+def test_cats():
+    assert data.CATS
